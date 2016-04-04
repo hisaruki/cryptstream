ba522ef43cf22507ac317d196f56bce0753ed4d6
hisaruki <hisaruki@pride.morningnegi.ddo.jp>
2016-04-05 02:15:50 +0900

    baidu成功

--- cryptstream.py
+++ cryptstream.py
@@ -10,7 +10,7 @@ class Upload:
     self.key = "zuSLNG5gzkHNvjPL"
     self.tmpdir = "/tmp"
 
-  def encode(self,bary,BLOCK_SIZE):
+  def AESenc(self,bary,BLOCK_SIZE):
     def pad(s,BLOCK_SIZE): 
       return s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * "{"
     cipher = AES.new(self.key)
@@ -31,47 +31,47 @@ class Upload:
         bary_size += len(buffer)
     yield bary
 
-  def create(self,func,enc=True):
+  def create(self,pre=None,post=None,enc=True):
     self.fingerprints = []
     for bary in self.recv():
       self.file = bary
       if enc:
-        self.file = self.encode(bary,32)
+        self.file = self.AESenc(bary,32)
       self.md5 = hashlib.md5(self.file).hexdigest()
       self.p = Path(self.tmpdir,self.md5)
+      if pre:pre()
       with self.p.open("wb") as f:
         f.write(self.file)
-      if func:
-        func()
+      if post:post()
       self.fingerprints.append(self.md5)
     self.p = Path(self.tmpdir,"index.json")
+    if pre:pre()
     with self.p.open("w") as f:
       f.write(json.dumps(self.fingerprints))
-      func()
-
-
-
+    if post:post()
 
 class Download:
   def __init__(self):
     self.key = "zuSLNG5gzkHNvjPL"
     self.tmpdir = "/tmp"
 
-  def decode(self,bin):
+  def AESdec(self,bin):
     cipher = AES.new(self.key)
     return base64.b64decode(cipher.decrypt(bin))
 
-  def create(self,func,dec=True):
-    with Path(self.tmpdir,"index.json").open("r") as f:
+  def create(self,pre=None,post=None,dec=True):
+    self.p = Path(self.tmpdir,"index.json")
+    if pre:pre()
+    with self.p.open("r") as f:
       self.fingerprints = json.loads(f.read())
+    if post:post()
     for fingerprint in self.fingerprints:
       self.p = Path(self.tmpdir,fingerprint)
-      if func:
-        func()
+      if pre:pre()
       with self.p.open("rb") as f:
         bin = f.read()
       self.file = bin
       if dec:
-        self.file = self.decode(bin)
+        self.file = self.AESdec(bin)
       sys.stdout.buffer.write(self.file)
-
+      if post:post()
--- upload.py
+++ upload.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import cryptstream,argparse,subprocess,sys
+from pathlib import Path
 
 
 parser = argparse.ArgumentParser(description="BaiduStreamer")
-parser.add_argument('name')
+parser.add_argument('path',type=str)
 parser.add_argument('--start',type=int)
 args = parser.parse_args()
 
 
 u = cryptstream.Upload()
 u.tmpdir = "/home/hisaruki/Desktop"
-u.divide = 20*1024
 
-def bypy():
-  """
+def byup():
+  dst = str( Path("/cryptstream/"+args.path) / u.p.name )
   proc = subprocess.Popen([
     "bypy",
     "upload",
-    str(u.p)
+    str(u.p),
+    dst
   ], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
   sys.stdout.write(proc[0].decode("utf-8"))
   u.p.unlink()
-  """
-u.create(bypy,True)
 
+u.create(post=byup)
 
 
 
