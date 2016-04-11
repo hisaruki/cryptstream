a73233db40d54c7485c5247b0f7c9d1c4e629d21
hisaruki <hisaruki@pride.morningnegi.ddo.jp>
2016-04-11 16:42:53 +0900

    再アップロード

--- upload.py
+++ upload.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import cryptstream,argparse,subprocess,sys,hashlib
+import cryptstream,argparse,subprocess,sys,hashlib,json
 from pathlib import Path
 
 
@@ -19,14 +19,30 @@ if args.key:
       u.key = hashlib.md5(f.read()).hexdigest()
 
 def byup():
-  dst = str( Path("/cryptstream/"+args.path) / u.p.name )
-  proc = subprocess.Popen([
-    "bypy",
-    "upload",
-    str(u.p),
-    dst
-  ], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-  sys.stdout.write(proc[0].decode("utf-8"))
+  if (not u.p.name in files) or (files[u.p.name]["valid"] == False):
+    proc = subprocess.Popen([
+      "bypy",
+      "upload",
+      str(u.p),
+      str( Path("/cryptstream/"+args.path) / u.p.name )
+    ], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    sys.stdout.write(proc[0].decode("utf-8"))
   u.p.unlink()
+
+ls = subprocess.Popen([
+  "bypy",
+  "ls",
+  str( Path("/cryptstream/"+args.path) )
+], stdout=subprocess.PIPE, stderr=None).communicate()
+files = {}
+for line in ls[0].decode("utf-8").split("\n"):
+  try:
+    data = line.split(" ")
+    files[data[1]] = {
+      "size":data[2],
+      "valid":(data[1]==data[5])
+    }
+  except:
+    ""
 
 u.create(post=byup,enc=hasattr(u,"key"))
